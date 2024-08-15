badfe2862912b4f1ed356db0a03e9a09b0c7e119
JongWoo Park <jjongwoo.dev@gmail.com>
2024-08-15 23:54:54 +0900

    initial commit

---
+++ src/main/java/kr/jongwow/spflux/SpfluxApplication.java
@@ -0,0 +1,13 @@
+package kr.jongwow.spflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/jongwow/spflux/SpfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jongwow.spflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
